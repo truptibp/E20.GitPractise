cb954298c5616dca2ffe12c4b47aacc8fb7bcd9a
Om Sai <Om Sai@DESKTOP-EP81H27>
2025-05-15 17:02:44 +0530

    Add method Committed.

--- src/test/java/Sample/Demo2.java
+++ src/test/java/Sample/Demo2.java
@@ -5,7 +5,17 @@ public class Demo2 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Git Hub Practise Demo");
+		
+		int value=add();
+		System.out.println("Addittion is= " + value);
 
+	}
+	
+	public static int add()
+	{
+		int a=10; int b=50;
+		int result= a + b;
+		return result;
 	}
 
 }
